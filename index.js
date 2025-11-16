1887a5cfbd165aeb3278f1c741162425d267b8a0
Rob Lord <robertlord@outlook.com>
2025-11-15 16:40:45 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
+require('datejs');
 
+function combineUsers(...args) {
+    const combinedObject = {
+        users: [],
+    };
+
+    for (let i = 0; i < args.length; i++) {
+        const currentArray = args[i];
+        combinedObject.users = [...combinedObject.users, ...currentArray];
+    }
+
+    // Today's date
+    combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+    return combinedObject;
+}
+
+module.exports = { combineUsers };
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
